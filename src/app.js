6c96c3783f07cb22504c699b4d3099f35c7b750f
Lucas Nascimento <lucasfortunato328@gmail.com>
2023-09-20 21:33:28 -0300

    images com uma barra só

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, '..', 'uploads', 'images'))); // Permite acessar arquivos estáticos, neste caso os de dentro da pasta "uploads"
+    this.app.use('/images', express.static(resolve(__dirname, '..', 'uploads', 'images'))); // Permite acessar arquivos estáticos, neste caso os de dentro da pasta "uploads"
   }
 
   routes() {
