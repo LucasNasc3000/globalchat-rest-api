91393b563fe113b5134d07a29e8906c5d4a28e2d
Lucas Nascimento <lucasfortunato328@gmail.com>
2023-10-28 13:28:41 -0300

    helmet

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,6 @@ _dotenv2.default.config();
 
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
-var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 var _home = require('./routes/home'); var _home2 = _interopRequireDefault(_home);
 var _user = require('./routes/user'); var _user2 = _interopRequireDefault(_user);
 var _token = require('./routes/token'); var _token2 = _interopRequireDefault(_token);
@@ -46,7 +45,6 @@ class App {
 
   middlewares() {
     this.app.use(_cors2.default.call(void 0, corsOptions));
-    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads'))); // Permite acessar arquivos estáticos, neste caso os de dentro da pasta "uploads"
--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
 import homeRoutes from './routes/home';
 import userRoutes from './routes/user';
 import tokenRoutes from './routes/token';
@@ -46,7 +45,6 @@ class App {
 
   middlewares() {
     this.app.use(cors(corsOptions));
-    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, '..', 'uploads'))); // Permite acessar arquivos estáticos, neste caso os de dentro da pasta "uploads"
