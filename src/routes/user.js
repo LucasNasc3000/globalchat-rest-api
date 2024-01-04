8349efec925639c3bab167702709fd75a0ce455c
lucas <lucasfortunato328@gmail.com>
2024-01-04 18:36:01 -0300

    routes3

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import userController from '../controllers/User'; // O home controller não é importado aqui com maiúsculo para não ser importado como objeto da classe em vez da classe em si, porque a classe já tem o 'h' maiúsculo
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
@@ -8,7 +9,7 @@ router.post('/', userController.store);
 // Estes dois não poderiam ser colocados em uma aplicação real
 router.get('/', userController.index); // Lista todos os usuários, com todos os seus dados
 
-router.put('/', userController.update);
-router.delete('/:id', userController.delete);
+router.put('/', loginRequired, userController.update);
+router.delete('/:id', loginRequired, userController.delete);
 
 export default router;
