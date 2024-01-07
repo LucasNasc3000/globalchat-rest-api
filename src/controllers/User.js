b7e4bd66fe39bcdb06cb3eaff9e8f3a65393c189
lucas <lucasfortunato328@gmail.com>
2024-01-07 12:11:20 -0300

    User3

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -56,6 +56,7 @@ export class UserController {
         where: {
           email: req.params.useremail,
         },
+        attributes: ['id', 'nome', 'email', 'isbanned'],
       });
 
       if (!userFind) {
