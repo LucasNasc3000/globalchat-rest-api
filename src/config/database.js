1ef6b4c27f6958982f0395d58507cde56a26527e
lucas <lucasfortunato328@gmail.com>
2024-01-09 16:03:16 -0300

    db config

--- src/config/database.js
+++ src/config/database.js
@@ -15,7 +15,7 @@ module.exports = {
     updatedAt: 'updated_at',
   },
   dialectOptions: {
-    timezone: '-03:00',
+    timezone: 'Brasil/East',
   },
-  timezone: '-03:00',
+  timezone: 'Brasil/East',
 };
