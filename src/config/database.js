5fa4742893d5c79fd47177c4378b079038fee4da
lucas <lucasfortunato328@gmail.com>
2024-01-09 20:37:31 -0300

    db config10

--- src/config/database.js
+++ src/config/database.js
@@ -14,5 +14,8 @@ module.exports = {
     createdAt: 'created_at',
     updatedAt: 'updated_at',
   },
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo',
+  },
   timezone: 'America/Sao_Paulo',
 };
