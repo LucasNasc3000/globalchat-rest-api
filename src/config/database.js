4d503d39e45e2e7dc894a786dd4853ec3b3c8fe9
lucas <lucasfortunato328@gmail.com>
2024-01-09 20:36:15 -0300

    db config9

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,6 @@ module.exports = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
-  dateStrings: true,
   define: {
     timestamps: true,
     underscored: true,
@@ -15,5 +14,5 @@ module.exports = {
     createdAt: 'created_at',
     updatedAt: 'updated_at',
   },
-  timezone: '-03:00',
+  timezone: 'America/Sao_Paulo',
 };
