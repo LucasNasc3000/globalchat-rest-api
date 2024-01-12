73a3f2779a51dfaa2d03c57189055bba986cb222
lucas <lucasfortunato328@gmail.com>
2024-01-12 13:57:27 -0300

    Text19

--- src/controllers/Text.js
+++ src/controllers/Text.js
@@ -25,7 +25,7 @@ class TextController {
 
   async index(req, res) {
     try {
-      const message = await Text.findAll({ attributes: ['textcontent', 'useremail', 'created_at', 'id', 'msghour'], order: ['id', 'DESC'] }); // O attributes lista somente os campos cujos nomes foram passados no array. Por segurança
+      const message = await Text.findAll({ attributes: ['textcontent', 'useremail', 'created_at', 'id', 'msghour'], order: [['id', 'DESC']] }); // O attributes lista somente os campos cujos nomes foram passados no array. Por segurança
       return res.json(message);
     } catch (e) {
       return res.json(null);
