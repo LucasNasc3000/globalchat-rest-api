15a7f8d53ffeff2714e215453233b0b9733c0218
lucas <lucasfortunato328@gmail.com>
2024-04-26 20:40:13 -0300

    agora com mj

--- dist/controllers/Emails.js
+++ dist/controllers/Emails.js
@@ -1,48 +1,32 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true});/* eslint-disable import/no-extraneous-dependencies */
-var _nodemailer = require('nodemailer');
+const mailer = require('nodemailer');
+const smtp = require('nodemailer-smtp-transport');
 
 class EmailsController {
   async SendEmail(req, res) {
-    try {
-      const transporter = _nodemailer.createTransport.call(void 0, {
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        service: 'gmail',
-        tls: {
-          rejectUnauthorized: false,
-        },
-        auth: {
-          type: 'OAuth2',
-          user: process.env.MAIL_USERNAME,
-          pass: process.env.MAIL_PASSWORD,
-          clientId: process.env.OAUTH_CLIENTID,
-          clientSecret: process.env.OAUTH_CLIENT_SECRET,
-          refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-        },
-      });
+    async function mailjet() {
+      const transport = mailer.createTransport(
+        smtp({
+          host: 'in.mailjet.com',
+          port: 2525,
+          auth: {
+            user: process.env.MAILJET_API_KEY,
+            pass: process.env.MAILJET_API_SECRET,
+          },
+        }),
+      );
 
-      const mailOptions = {
-        from: req.body.emailFrom,
+      const json = await transport.sendMail({
+        from: req.body.from,
         to: req.body.destinatary,
-        subject: 'Nodemailer Project2',
-        text: 'Testando',
-      };
-
-      transporter.sendMail(mailOptions, (err, data) => {
-        if (err) {
-          console.log(`Error ${err}`);
-        } else {
-          console.log('Email sent successfully');
-          console.log(data);
-        }
+        subject: 'test email from Node.js on Google Cloud Platform',
+        text: 'Hello!\n\nThis a test email from Node.js.',
+        html: '<h1>Opa, tudo bom?<h1/>',
       });
-      return res.json('Email enviado com sucesso');
-    } catch (e) {
-      return res.status(400).json({
-        errors: e.errors.map((err) => err.message),
-      });
+      console.log(json);
+      res.json({ json });
     }
+    mailjet();
   }
 }
 
