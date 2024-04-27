a6b0b6eff55b12782c9a5bdcfb610e7c497c79fd
lucas <lucasfortunato328@gmail.com>
2024-04-27 17:05:58 -0300

    mj2

--- src/controllers/Emails.js
+++ src/controllers/Emails.js
@@ -8,7 +8,7 @@ class EmailsController {
       const transport = mailer.createTransport(
         smtp({
           host: 'in.mailjet.com',
-          port: 2525,
+          port: 587,
           auth: {
             user: process.env.MAILJET_API_KEY,
             pass: process.env.MAILJET_API_SECRET,
