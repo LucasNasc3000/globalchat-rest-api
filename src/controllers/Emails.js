7acd339c3ec15c9a6d2b5c93eb652dc811d014e3
lucas <lucasfortunato328@gmail.com>
2024-05-02 20:11:43 -0300

    testing sg

--- src/controllers/Emails.js
+++ src/controllers/Emails.js
@@ -1,35 +1,22 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
-const mailer = require('nodemailer');
-const smtp = require('nodemailer-smtp-transport');
+const sendgrid = require('@sendgrid/mail');
 
 class EmailsController {
   async SendEmail(req, res) {
-    async function mailjet() {
-      const transport = mailer.createTransport(
-        smtp({
-          host: 'in.mailjet.com',
-          port: 465,
-          tls: {
-            rejectUnauthorized: false,
-          },
-          auth: {
-            user: process.env.MAILJET_API_KEY,
-            pass: process.env.MAILJET_API_SECRET,
-          },
-        }),
-      );
+  // This sample is based off of:
+  // https://github.com/sendgrid/sendgrid-nodejs/tree/master/packages/mail
+    sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-      const json = await transport.sendMail({
-        from: req.body.from,
+    async function sendgridExample() {
+      await sendgrid.send({
         to: req.body.destinatary,
-        subject: 'test email from Node.js on Google Cloud Platform',
-        text: 'Hello!\n\nThis a test email from Node.js.',
-        html: '<h1>Opa, tudo bom?<h1/>',
+        from: 'lucasnascdev@gmail.com',
+        subject: 'Sendgrid test email from Node.js on Google Cloud Platform',
+        text: 'Well hello! This is a Sendgrid test email from Node.js on Google Cloud Platform.',
       });
-      console.log(json);
-      res.json({ json });
     }
-    mailjet();
+    sendgridExample();
   }
 }
 
