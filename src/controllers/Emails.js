42d33f52be026bd5cb3bd859ae722ebe8b553f92
lucas <lucasfortunato328@gmail.com>
2024-05-15 18:11:53 -0300

    testsg

--- src/controllers/Emails.js
+++ src/controllers/Emails.js
@@ -1,23 +1,25 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/no-extraneous-dependencies */
-const sendgrid = require('@sendgrid/mail');
+import dotenv from 'dotenv';
 
-class EmailsController {
-  SendEmail(req, res) {
-    // This sample is based off of:
-    // https://github.com/sendgrid/sendgrid-nodejs/tree/master/packages/mail
-    sendgrid.setApiKey('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');
+dotenv.config();
 
-    async function sendgridExample() {
-      await sendgrid.send({
-        to: req.body.destinatary,
-        from: 'storagemail329@gmail.com',
-        subject: 'Sendgrid test email from Node.js on Google Cloud Platform',
-        text: 'Well hello! This is a Sendgrid test email from Node.js on Google Cloud Platform.',
-      });
-    }
-    sendgridExample();
-  }
-}
+const sgMail = require('@sendgrid/mail');
 
-export default new EmailsController();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const msg = {
+  to: 'storagemail329@gmail.com', // Change to your recipient
+  from: 'lucasnascdev@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+
+sgMail
+  .send(msg)
+  .then((response) => {
+    console.log(response[0].statusCode);
+    console.log(response[0].headers);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
