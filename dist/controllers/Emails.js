f6c22d6df0195a4f35b4fc683aff978e1560dd2b
lucas <lucasfortunato328@gmail.com>
2024-05-15 18:15:40 -0300

    testsg2

--- dist/controllers/Emails.js
+++ dist/controllers/Emails.js
@@ -1,25 +1,32 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 
 _dotenv2.default.config();
 
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+class EmailsController {
+  SendEmail(req, res) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const msg = {
-  to: 'storagemail329@gmail.com', // Change to your recipient
-  from: 'lucasnascdev@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
+    const msg = {
+      to: 'storagemail329@gmail.com', // Change to your recipient
+      from: req.body.destinatary, // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
 
-sgMail
-  .send(msg)
-  .then((response) => {
-    console.log(response[0].statusCode);
-    console.log(response[0].headers);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+    sgMail
+      .send(msg)
+      .then((response) => {
+        console.log(response[0].statusCode);
+        console.log(response[0].headers);
+        res.json('Email enviado');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
+
+exports. default = new EmailsController();
--- src/controllers/Emails.js
+++ src/controllers/Emails.js
@@ -4,22 +4,29 @@ dotenv.config();
 
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+class EmailsController {
+  SendEmail(req, res) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const msg = {
-  to: 'storagemail329@gmail.com', // Change to your recipient
-  from: 'lucasnascdev@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
+    const msg = {
+      to: 'storagemail329@gmail.com', // Change to your recipient
+      from: req.body.destinatary, // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
 
-sgMail
-  .send(msg)
-  .then((response) => {
-    console.log(response[0].statusCode);
-    console.log(response[0].headers);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+    sgMail
+      .send(msg)
+      .then((response) => {
+        console.log(response[0].statusCode);
+        console.log(response[0].headers);
+        res.json('Email enviado');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
+
+export default new EmailsController();
